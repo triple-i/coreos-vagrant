01d39255339f155ba968ac4befc43c6ccb42f0a4
Skobayashi <suguru@iii-planning.com>
2015-03-30 13:41:43 +0900

    Vagrant初期化時に生成するDockerイメージをPackerから生成するように改善した

--- share/bootstrap.sh
+++ share/bootstrap.sh
@@ -1,8 +1,16 @@
 #! /usr/bin/env bash
 
-# FedoraのDockerイメージを取得
-docker pull fedora:21
+# Packerをダウンロードする
+# docker上でpackerを実行するとバグるため修正版のpackerを使用する
+# https://github.com/mitchellh/packer/issues/1752
+git clone https://github.com/triple-i/packer-issue1752-fix.git packer
 
-cd /home/core/share/TripleI.ServerConfigs/docker/Fedora/
-docker build -t triplei/core:latest .
+unlink /home/core/.bashrc
+echo "export PATH=$PATH:/home/core/packer/packer" >> /home/core/.bashrc
+source /home/core/.bashrc
+
+
+# Packerを実行してDockerイメージを構築する
+cd /home/core/share/TripleI.ServerConfigs/triplei-core
+packer build ./packer/docker_image.json
 
